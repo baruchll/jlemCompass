cceec244911591811a192491130b05e7f1f25f8a
Baruch <benmlehrfeld@gmail.com>
2021-08-19 16:26:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/jlemcompass/CompassActivity.java
@@ -0,0 +1,65 @@
+package com.example.jlemcompass;
+
+import static android.view.View.INVISIBLE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CompassActivity extends AppCompatActivity {
+    private static final String TAG = CompassActivity.class.getSimpleName();
+    private Compass compass;
+    private ImageView jlemIndicator;
+    private ImageView imageDial;
+    private TextView tvAngle;
+    private TextView tvYourLocation;
+    private float currentAzimuth;
+
+    SharedPreferences prefs;
+    GPSTracker gps;
+    private final int RC_Permission = 1221;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+
+        setUserChanges(getIntent());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(ContextCompat.getDrawable(this, R.drawable.ic_arrow_back));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        /////////////////////////////////////////////////
+        prefs = getSharedPreferences("", MODE_PRIVATE);
+        gps = new GPSTracker(this);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //////////////////////////////////////////
+        qiblatIndicator = findViewById(R.id.qibla_indicator);
+        imageDial = findViewById(R.id.dial);
+        tvAngle = findViewById(R.id.angle);
+        tvYourLocation = findViewById(R.id.your_location);
+
+        //////////////////////////////////////////
+        qiblatIndicator.setVisibility(INVISIBLE);
+        qiblatIndicator.setVisibility(View.GONE);
+
+        setupCompass();
+    }
+
+    private void setUserChanges(Intent intent) {
+    }
+}
+}
